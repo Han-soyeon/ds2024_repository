5bdd92788e88f51b5ad5def2690c478c8dc0dfcd
Han-soyeon <20227003@edu.hanbat.ac.kr>
2024-03-21 12:34:24 +0900

    자료 구조 1주차

---
+++ week1_project/src/week1_project/data_structure.java
@@ -0,0 +1,10 @@
+package week1_project;
+
+public class data_structure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+
+}
