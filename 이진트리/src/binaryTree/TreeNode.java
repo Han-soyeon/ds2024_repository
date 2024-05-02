41dc675d78362b0b9cf9e941bdc4693a32c81347
Han-soyeon <20227003@edu.hanbat.ac.kr>
2024-05-02 10:55:50 +0900

    이진트리 구현

---
+++ 이진트리/src/binaryTree/TreeNode.java
@@ -0,0 +1,19 @@
+package binaryTree;
+
+public class TreeNode<E> {
+	E item;
+	TreeNode<E> left;
+	TreeNode<E> right;
+
+	// 트리 노드를 초기화할 생성자 생성
+	public TreeNode(final E newItem, final TreeNode<E> left, final TreeNode<E> right) {
+		this.item = newItem;
+		this.left = this.right = null;
+		this.right = this.right = null;
+	}
+	
+	// 위의 생성자를 통해 트리를 만든다.
+
+	// getter & setter
+
+}
